8e282d3797bd6c76d4bfd3ff759104c07ddcf897
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2026-04-28 10:48:13 +0200

    Add

--- src/ynov_m2_sysemb_demo/statistics_demo.py
+++ src/ynov_m2_sysemb_demo/statistics_demo.py
@@ -1,13 +1,21 @@
 import statistics
 from pathlib import Path
+from turtle import st
 
 from .file import CsvFile
 
 def main():
     csv_file = CsvFile()
     data = csv_file.load(Path("telemetries.csv"))
-    for line in data:
-        print(line["Time"])
+
+    time_x = [float(line["Time"]) for line in data]
+    rpm_y = [float(line["RPM"]) for line in data]
+    # print(rpm_y)
+
+    print(statistics.mean(rpm_y))
+    print(min(rpm_y))
+    print(max(rpm_y))
+    print(statistics.median(rpm_y))
     
 
 if (__name__ == "__main__"):
